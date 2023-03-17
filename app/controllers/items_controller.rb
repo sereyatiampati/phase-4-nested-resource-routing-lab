5bc6dad6a7c0d49e2f66a79b454d8f23cf3edce7
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-17 10:20:08 +0300

    nested routing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user=User.find_by(id: params[:user_id])
+      if user
+        render json: user.items
+      else
+        render json: {error: "user not found"}, status: :not_found
+      end
+      
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    item=Item.find_by(id: params[:id])
+    if item
+      render json: item
+    else
+      render json: {error: "item not found"}, status: :not_found
+    end
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show]
+  end
+  resources :items, only: [:index, :show]
 end
